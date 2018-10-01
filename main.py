b545b13c125ca3bf52f6f8c391fc1cfb956c1fa0
Kevin Fausett <fausett.k@husky.neu.edu>
2018-10-01 08:58:09 -0400

    Added support for variable n-grams

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ def parseJsonToNGram(data, n):
                 if 'content' in message and len(message['content']) > n:
                     content = message['content'].split(" ")
                     for i in range(n, len(content)):
-                        key = tuple([content[i - k] for k in range(n, 0, -1)])
-                        # key = content[i - 2], content[i - 1]
+                        if n > 1:
+                            key = tuple([content[i - k] for k in range(n, 0, -1)])
+                        else:
+                            key = content[i-1]
                         if key in transitionTables[person]:
                             transitionTables[person][key].append(content[i])
                         else:
@@ -24,31 +26,37 @@ def parseJsonToNGram(data, n):
     return transitionTables
 
 
-def generateChain(transitionTables, data):
+def generateChain(transitionTables, data, n):
     for participant in data['participants']:
         person = participant['name']
         print(person)
         start = choice(list(transitionTables[person].keys()))
-        word1 = start[0]
-        word2 = start[1]
-        word3 = choice(transitionTables[person][start])
+        words = [start[i] for i in range(0, n)]
+        words.append(choice(transitionTables[person][start]))
         res = []
-        res.extend((word1, word2, word3))
-        i = 0
-        for i in range(0, 100):
-            key = word2, word3
+        res.extend(words)
+        t = 0
+        f = 0
+        for count in range(0, 100):
+            if n > 1:
+                key = tuple(words[1:])
+            else:
+                key = words[1]
             if key in transitionTables[person]:
-                word1 = word2
-                word2 = word3
-                word3 = choice(transitionTables[person][word1, word2])
-                res.append(word3)
+                for i in range(0, len(words) - 1):
+                    words[i] = words[i+1]
+                words[-1] = choice(transitionTables[person][key])
+                res.append(words[-1])
+
             else:
                 key = choice(list(transitionTables[person].keys()))
-                word1 = key[0]
-                word2 = key[1]
-                word3 = choice(transitionTables[person][key])
-                res.extend((word1, word2, word3))
-            i += 1
+                if n > 1:
+                    words = [key[i] for i in range(0, n)]
+                else:
+                    words = [key]
+                words.append(choice(transitionTables[person][key]))
+                res.extend(words)
+            count += 1
         resStr = ' '.join(res)
         filename = participant['name'] + ".txt"
         pathlib.Path(filename).write_text(resStr, encoding="utf8")
@@ -57,8 +65,8 @@ def generateChain(transitionTables, data):
 def main():
     with open('message.json') as messageJSON:
         data = json.loads(messageJSON.read())
-    tt = parseJsonToNGram(data, 2)
-    generateChain(tt, data)
+    tt = parseJsonToNGram(data, 3)
+    generateChain(tt, data, 3)
 
 
 main()
