18b139d00b38f508654a88189e5bf53085f95a35
Kevin Fausett <kevin.fausett@gmail.com>
2018-10-05 11:27:29 -0400

    Prior probability

--- classifier.py
+++ classifier.py
@@ -4,58 +4,52 @@ from functools import reduce
 
 def setupTable(data):
         table = {}
-        # If we keep track of total message counts per user in the same dict we risk a collision
-        counts = {}
+        # Per user word and message counts
+        counts = {'word': {}, 'message': {}}
         # Ignore messages from people we excommunicated
         currentParticipants = []
-        table['totalcount'] = 0
+        table['totalmessages'], table['vocabularylen'] = 0, 0
         for participant in data['participants']:
             person = participant['name']
             currentParticipants.append(person)
             table[person] = {}
-            counts[person] = 0
+            counts['word'][person], counts['message'][person] = 0, 0
         for message in data['messages']:
             person = message['sender_name']
             if person in currentParticipants and 'content' in message:
-                table['totalcount'] += 1
-                counts[person] += 1
+                table['totalmessages'] += 1
+                counts['message'][person] += 1
                 content = message['content'].split(" ")
                 for word in content:
+                    counts['word'][person] += 1
                     if word in table[person]:
                         table[person][word] += 1
                     else:
                         table[person][word] = 1
+                        table['vocabularylen'] += 1
         return table, counts, currentParticipants
 
 
 def naiveBayes(table, counts, currentParticipants, text):
     words = text.split(" ")
     scores = {}
-    participantCount = len(currentParticipants)
     for person in currentParticipants:
         scores[person] = []
     for word in words:
         temp = {}
         # laplace smoothing
-        total = participantCount
+        v = table['vocabularylen']
         for person in currentParticipants:
             if word in table[person]:
                 occurrences = table[person][word] + 1
             else:
                 occurrences = 1
-            temp[person] = occurrences
-            total += occurrences
-        for person in currentParticipants:
-            probability = (temp[person] + 1) / total
-            scores[person].append(probability)
 
-    # We may or may not wan to weight prior probabilities. If one person posts more frequently in the groupchat,
-    # a truly randomly selected message from the test set IS more likely to be them. But if we're just submitting quotes
-    # that humans arbitrarily picked, we may want all prior probabilities to be weighed equally. We will output weighted
-    # (including prior probability) and unweighted results.
+            scores[person].append(occurrences / (counts['word'][person] + v))
+
     wtScores, unwtScores = {}, {}
     for person in currentParticipants:
-        prior = counts[person] / table['totalcount']
+        prior = counts['message'][person] / table['totalmessages']
         totalUnwtProb = reduce((lambda x, y: x * y), scores[person])
         scores[person].append(prior)
         totalWtProb = reduce((lambda x, y: x * y), scores[person])
@@ -67,7 +61,6 @@ def naiveBayes(table, counts, currentParticipants, text):
     print(max(unwtScores.items(), key=lambda item: item[1]))
     print("Max weighted:")
     print(max(wtScores.items(), key=lambda item: item[1]))
-
 
 def main():
     with open('message.json') as messageJSON:
--- readme.md
+++ readme.md
@@ -28,15 +28,29 @@ Features:
 * Predict most likely speaker of a message based off their previous messages 
 * Naive Bayes implementation using FB messenger chats
 * Laplace Smoothing to handle unseen words
-* Returns result weighted by prior probability as well as unweighted
+* Returns result weighted by prior probability as well as unweighted (rationality explained below)
 
 To use: same as above.
 
 Output: The most likely speaker of a given quote based off the unweighted score, the most likely speaker based off the weighted score, and the corresponding scores printed to console.
 The program will then prompt for another quote to classify until the user types "QUIT" or kills the process.
-The inclusion of weighted and unweighted scores is because if you randomly select a facebook message from outside the training set, the prior probability of the 
-likelihood of a particular person speaking should be factored in. However, there are circumstances where we may want to guess the speaker without factoring in
-the amount of time someone spends speaking (seeing who would be most likely to say a quote that was not actually said, humans selecting their favorite quotes someone has said, etc)
+
+We may or may not want to weight prior probabilities (by number of messages a participant has sent divided by the total 
+number of messages as opposed to saying prior probability is 1/number of participants) depending on the nature 
+of the message. If one person posts more frequently in the groupchat,
+a truly randomly selected message from the test set IS more likely to be them. But if we're just submitting quotes
+that humans arbitrarily picked, we may want all prior probabilities to be weighted equally. However, if you do not
+weight the result and the test set includes universally unseen words, it may be unfairly biased towards someone who has
+spoken fewer words. After all, if you've never said a word and it has "one" occurrence (laplace smoothing) out of 100,000
+words you've said compared to "one" occurrence out of 5,000 words someone else has said, it would rate the person
+who has spoken less as more likely to say it. Thus, we will output weighted (including prior probability) and
+unweighted results. Ideally, sending more messages means sending more words, and thus the higher count of words
+in that class resulting in a lower probability for a given word will balance out the higher probability to be 
+accurate but people have different tendencies in how they break up messages (breaking a paragraph into multiple vs
+one for example) so someone who sends a higher than average number of messages but fewer than average words would 
+likely be seen as the most likely speaker for any brand new words.
+
+As a default, the weighted result should be the preferred, but in some contexts the unweighted may be taken into consideration.
 
 To-do:
 * Potentially add results from more classifiers.
